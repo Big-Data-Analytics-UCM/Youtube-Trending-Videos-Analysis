c3e2814dd267aa143850c5dec5d088b90c232945
Camilo <cdisidoro99@gmail.com>
2021-12-07 18:05:33 +0100

    Videos mas comentados (Capado a los 100 mas comentados de momento)

---
+++ most_commented.py
@@ -0,0 +1,32 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+countries = ["BR", "CA", "DE", "FR", "GB", "IN", "JP", "KR", "MX", "RU", "US"]
+
+def get_comment_count(country):
+    ruta_csv = 'data/' + country + '_youtube_trending_data.csv'
+    ruta_json = 'data/' + country + '_category_id.json'
+
+    df = pd.read_csv(ruta_csv)
+    df = df[['title', 'comment_count']]
+    mask = (df.comment_count <= 0)
+    df = df.loc[~mask]
+
+    df = df.sort_values(by=['comment_count'], ascending=False).drop_duplicates(subset=['title'])
+    res = df.head(100)
+    print(res.to_string(index = False))
+
+
+if __name__ == "__main__":
+    # Arg Parser
+    import argparse
+    parser = argparse.ArgumentParser()
+    helpRegionCode = 'Region code for the youtube videos, by default ALL.\nPossible regions:\nCA: Canada,\n\tDE: Alemania,\n\tFR: Francia,\n\tGB: Reino Unido,\n\tIN: India,\n\tJP: Japon,\n\tKR: Korea,\n\tMX: Mexico,\n\tRU: Rusia,\n\tUS: Estados Unidos'
+    parser.add_argument("regionCode", help=helpRegionCode, default="ALL")
+    parser.add_argument("-m", "--mode", help='console or graph, by default is console', default="console")
+    args = parser.parse_args()
+    # END OF ARGUMENT PARSER
+    region = args.regionCode.upper()
+
+    get_comment_count(region)
