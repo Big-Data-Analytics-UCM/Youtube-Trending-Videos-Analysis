53de696d12e2993b23c34756b9c5d37363500483
Camilo Andres D'isidoro <cdisidoro99@gmail.com>
2021-12-12 18:59:53 +0100

    MoreTags

--- scripts/more_tags.py
+++ scripts/more_tags.py
@@ -53,6 +53,10 @@ def get_table(regionDF, region):
     outFile.write(table)
     outFile.close()
 
+def get_local(region):
+    regionDf = get_tags(region)
+    print(regionDf.to_string(index=False))
+    get_table(regionDf, region)
 
 def get_global():
     global_df = pd.DataFrame()
@@ -63,6 +67,7 @@ def get_global():
     global_df = global_df.sort_values(by=['view_count'], ascending=False).head(10)
     global_df = global_df.reset_index(drop=True)
     get_table(global_df, "GLOBAL")
+    print(global_df.to_string(index=False))
 
 
 if __name__ == "__main__":
@@ -81,7 +86,4 @@ if __name__ == "__main__":
     if region == "GLOBAL":
         get_global()
     else:
-        get_table(get_tags(region), region)
-
-
-
+        get_local(region)
