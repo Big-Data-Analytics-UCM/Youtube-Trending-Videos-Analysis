920a92abc4741a42b8d2b4dd55d8a94456893f79
Camilo Andres D'isidoro <cdisidoro99@gmail.com>
2021-12-12 15:05:52 +0100

    Salida mejorada para Top10 Visitas

--- top10_most_views.py
+++ top10_most_views.py
@@ -11,13 +11,35 @@ def get_view_count(country):
     df = df.loc[~mask]
 
     df = df.sort_values(by=['view_count'], ascending=False).drop_duplicates(subset=['title'])
-    res = df.head(10)
+    res = df.head(10).reset_index(drop = True)
     return res
 
 
-def get_local(region):
-    print(get_view_count(region).to_string(index=False))
-
+def get_table(regionDF, region):
+    #print(get_view_count(region).to_string(index=False))
+    #Creates the HTML Struct for the output file
+    table = "<html>\n"
+    table+= "<head>\n"
+    #Bootstrap Linking
+    table+= "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n"
+    table+= "</head>\n"
+    table+= "<body>\n"
+    table+="<section class=\"container-md\">\n"
+    #Converting output DataFrame to a HTML table, and stilizing with bootstrap
+    dfOut = regionDF.to_html()
+    dfOut = dfOut.replace("<table border=\"1\" class=\"dataframe\">", "<table class=\"table table-dark table-striped\">")
+    dfOut = dfOut.replace("<tr style=\"text-align: right;\">", "<tr>")
+    dfOut = dfOut.replace("<th>title</th>", "<th>Título del vídeo</th>")
+    dfOut = dfOut.replace("<th>view_count</th>", "<th>Visitas totales</th>")
+    dfOut = dfOut.replace("<th>region</th>", "<th>Región</th>")
+    table+= dfOut
+    table+="</section>\n"
+    table+= "</body>\n"
+    table+= "</html>\n"
+    #Writes the final HTML code
+    outFile = open("outData/top10_" + region + ".html", "w")
+    outFile.write(table)
+    outFile.close()
 
 def get_global():
     global_df = pd.DataFrame()
@@ -26,7 +48,8 @@ def get_global():
         region_df = region_df.assign(region = [reg, reg, reg, reg, reg, reg, reg, reg, reg, reg])
         global_df = global_df.append(region_df, ignore_index=True)
     global_df = global_df.sort_values(by=['view_count'], ascending=False).drop_duplicates(subset=['title']).head(10)
-    print(global_df.to_string(index=False))
+    global_df = global_df.reset_index(drop = True)
+    get_table(global_df, "GLOBAL")
 
 
 if __name__ == "__main__":
@@ -42,4 +65,4 @@ if __name__ == "__main__":
     if region == "GLOBAL":
         get_global()
     else:
-        get_local(region)
+        get_table(get_view_count(region), region)
