1720cb5ec7aae0343ac8e4785f8d7890b8afeadd
AlvaroPlaza <57074350+AlvaroPlaza@users.noreply.github.com>
2021-12-12 18:41:25 +0100

    mejora funcion tags global

--- codes/more_tags.py
+++ codes/more_tags.py
@@ -26,7 +26,42 @@ def get_tags(country):
 
     df = pd.DataFrame([[key, dic[key]] for key in dic.keys()], columns=['tags', 'view_count'])
     df = df.sort_values(by=['view_count'], ascending=False).head(10)
-    print(df)
+    return df.head(10).reset_index(drop=True)
+
+def get_table(regionDF, region):
+    #Creates the HTML Struct for the output file
+    table = "<html>\n"
+    table+= "<head>\n"
+    #Bootstrap Linking
+    table+= "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n"
+    table+= "</head>\n"
+    table+= "<body>\n"
+    table+="<section class=\"container-md\">\n"
+    #Converting output DataFrame to a HTML table, and stilizing with bootstrap
+    dfOut = regionDF.to_html()
+    dfOut = dfOut.replace("<table border=\"1\" class=\"dataframe\">", "<table class=\"table table-dark table-striped\">")
+    dfOut = dfOut.replace("<tr style=\"text-align: right;\">", "<tr>")
+    dfOut = dfOut.replace("<th>title</th>", "<th>Nombre del tag</th>")
+    dfOut = dfOut.replace("<th>likes</th>", "<th>Número de visualizaciones</th>")
+    dfOut = dfOut.replace("<th>region</th>", "<th>Región</th>")
+    table+= dfOut
+    table+="</section>\n"
+    table+= "</body>\n"
+    table+= "</html>\n"
+    #Writes the final HTML code
+    outFile = open("outData/more_tags_" + region + ".html", "w")
+    outFile.write(table)
+    outFile.close()
+
+def get_global():
+    global_df = pd.DataFrame()
+    for reg in countries:
+        region_df = get_tags(reg)
+        region_df = region_df.assign(region=[reg, reg, reg, reg, reg, reg, reg, reg, reg, reg])
+        global_df = global_df.append(region_df, ignore_index=True)
+    global_df = global_df.sort_values(by=['view_count'], ascending=False).head(10)
+    global_df = global_df.reset_index(drop=True)
+    get_table(global_df, "GLOBAL")
 
 
 if __name__ == "__main__":
@@ -38,7 +73,12 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--mode", help='console or graph, by default is console', default="console")
     args = parser.parse_args()
     # END OF ARGUMENT PARSER
-    country = args.regionCode.upper()
+    region = args.regionCode.upper()
 
-    get_tags(country)
+    if region == "GLOBAL":
+        get_global()
+    else:
+        get_table(get_tags(region), region)
+
+
 
